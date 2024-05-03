58f3786d461628ee9164b472d1c1aca5bfdab462
juliangrtz <julian.goeritz@student.uni-luebeck.de>
2024-05-03 16:06:39 +0200

    Add Messaging::writeHeader offset

--- agent/offsets.ts
+++ agent/offsets.ts
@@ -9,6 +9,7 @@ export class Offsets {
     static MESSAGING_ONRECEIVE = idaAddressToMemoryAddress(ptr("0x100408C2C"));
     static MESSAGING_SEND = idaAddressToMemoryAddress(ptr("0x100D6F23C"));
     static MESSAGING_SEND_PEPPER_LOGIN = idaAddressToMemoryAddress(ptr("0x100D7DD94"));
+    static MESSAGING_WRITE_HEADER = idaAddressToMemoryAddress(ptr("0x100D70F30"));
     static PEPPER_CRYPTO_BOX_OPEN = idaAddressToMemoryAddress(ptr("0x1001ACC04"));
     static PEPPER_CRYPTO_BOX = idaAddressToMemoryAddress(ptr("0x100D7DE44"));
     static PEPPER_ENCRYPTER_ENCRYPT = idaAddressToMemoryAddress(ptr("0x100D7DEEC"));
